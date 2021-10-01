a6f49add52bbf18657dc137d855c48c45e37c015
Fahim Faisal <fahimfaisal1998@gmail.com>
2021-10-01 16:08:57 +0600

    Lab Task-1 Portfolio

---
+++ Portfolio/Portfolio/Controllers/ProfileController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Bio()
+        {
+            return View();
+        }
+        public ActionResult Educations()
+        {
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            return View();
+        }
+        public ActionResult References()
+        {
+            return View();
+        }
+    }
+}
